cd98d99777ddaf2a9e4cab526268c22c8a84f525
Priel Krishtal <prielkr@mta.ac.il>
2026-02-28 02:27:25 +0200

    feat: setup User model and configure bidirectional one-to-many relationship with receipts

--- app/db/models.py
+++ app/db/models.py
@@ -1,9 +1,9 @@
 from app.db.database import Base
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Numeric,func
+from sqlalchemy.orm import Mapped, mapped_column,relationship
+from sqlalchemy import Numeric,func,String,Boolean,ForeignKey
 from decimal import Decimal      
 from datetime import date,datetime
-from typing import Optional
+from typing import Optional,List
 
 class Receipt(Base):
     __tablename__ = "receipts" 
@@ -15,8 +15,26 @@ class Receipt(Base):
     # Creation date: Set once by the server, never updates
     uploaded_at: Mapped[datetime] = mapped_column(server_default=func.now())
     processed_at: Mapped[Optional[datetime]]
-    
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped["User"] = relationship(back_populates="receipts") #for fetching User obj
     # Update date: Set on creation, auto-updates on every change
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), 
+        onupdate=func.now()
+    )
+
+
+class User(Base):
+    __tablename__ = "users" 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str] = mapped_column(String(255), unique=True,index=True)
+    hashed_password: Mapped[str] 
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    # Creation date: Set once by the server, never updates
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    receipts: Mapped[List["Receipt"]] = relationship(back_populates="owner")
+    
+
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), 
         onupdate=func.now()
